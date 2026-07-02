ff5680154269919f6aa0f155001fef5311a13bd2
Gaby Obando <rgobandon@gmail.com>
2026-07-02 18:51:12 -0500

    cambios

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,7 @@
+
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://eshwqowvtgepniomytmy.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_7OTPkH0A2nd7vInTywowQw_s8-yrJpS'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzaHdxb3d2dGdlcG5pb215dG15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzMDU5OTMsImV4cCI6MjA5Njg4MTk5M30.I1fesHE6p6hSl4CpOCS_24HikRdZlmXXOkUBPjGZOwY'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
